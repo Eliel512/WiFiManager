d7595b51c6fbaa8b8f7d60ed4cb5647b52c33272
Eliel512 <76760122+Eliel512@users.noreply.github.com>
2023-01-03 04:17:00 +0100

    Update wifimgr.py

--- wifimgr.py
+++ wifimgr.py
@@ -3,8 +3,8 @@ import socket
 import ure
 import time
 
-ap_ssid = "WifiManager"
-ap_password = "tayfunulu"
+ap_ssid = "ESP--AP"
+ap_password = "micropythoN"
 ap_authmode = 3  # WPA2
 
 NETWORK_PROFILES = 'wifi.dat'
